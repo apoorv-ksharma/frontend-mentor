5dbc9b91c13395bd5665648d43768b504ac0ee36
Apoorv Sharma <apoorv.kumar.sharma@gmail.com>
2024-09-24 21:59:54 +0530

    Links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className='p-10 flex gap-2'>
+      <Link className='p-2 bg-sky-500 rounded-md text-white' href='/age-calulator'>
+        Age Calulator
+      </Link>
+      <Link className='p-2 bg-sky-500 rounded-md text-white' href='/blog-preview-card'>
+        Blog Preview Card
+      </Link>
+    </div>
+  );
 }
